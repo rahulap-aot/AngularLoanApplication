de8c33d7b8aae0c6dc1ab05fbbd4ad4ce53afc2c
rahulap-aot <rahul.ap@aot-technologies.com>
2024-11-23 04:46:51 -0800

    lone done

--- src/app/components/loan-application/loan-application.component.ts
+++ src/app/components/loan-application/loan-application.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { Application, Loan } from '../../model/application.model';
 @Component({
   selector: 'app-loan-application',
-  imports: [],
+  imports: [ FormsModule ],
   templateUrl: './loan-application.component.html',
   styleUrl: './loan-application.component.css'
 })
 export class LoanApplicationComponent {
+
+  Application: Application =new Application();
+  loan: Loan = new Loan();
+
+  addLoan(){
+    const strobj= JSON.stringify(this.loan);
+    const newobj=JSON.parse(strobj);
+    this.Application.Loans.unshift(newobj);
+  }
 
 }
